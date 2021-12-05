b7e689ef8e38fa3b5ab08e71f00280b390ea61ae
Sergio De Luca <sdlbsso@gmail.com>
2021-12-05 19:26:32 -0300

    Filtrado

--- cryptoprecios/src/App.vue
+++ cryptoprecios/src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <h2>Mercado de criptomonedas</h2>
+      <input
+        type="text"
+        class="form-control bg-dark text-light rounded-0 border-0 my-4"
+        placeholder="Buscar mondeda"
+        v-model="textSearch"
+        @keyup="searchCoin()"
+      />
+      <table class="table table-dark">
+        <thead>
+          <tr>
+            <th v-for="titulo in titles" :key="titulo">{{ titulo }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(coin, index) in filteredCoins" :key="coin.id">
+            <td class="text-muted">{{ index + 1 }}</td>
+            <td>
+              <img :src="coin.image" style="width: 1rem" class="me-2" />
+              <span>{{ coin.name }}</span>
+              <span class="ms-2 text-uppercase text-muted"
+                >{{ coin.symbol }}
+              </span>
+            </td>
+            <td>$ {{ coin.current_price }}</td>
+            <td
+              :class="[
+                coin.price_change_percentage_24h > 0
+                  ? 'text-success'
+                  : 'text-danger',
+              ]"
+            >
+              {{ coin.price_change_percentage_24h }}%
+            </td>
+            <td>$ {{ coin.total_volume.toLocaleString() }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      coins: [],
+      filteredCoins: [],
+      titles: ["#", "Moneda", "Precio", "Cambio de precio", "Volumen 24hs"],
+      textSearch: "",
+    };
+  },
+  async mounted() {
+    const res = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+    );
+    const data = await res.json();
+    this.coins = data;
+    this.filteredCoins = data;
+  },
+  methods: {
+    searchCoin() {
+      this.filteredCoins = this.coins.filter(
+        (coin) =>
+          coin.name.toLowerCase().includes(this.textSearch.toLowerCase()) ||
+          coin.symbol.toLowerCase().includes(this.textSearch.toLowerCase())
+      );
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
